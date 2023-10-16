26e147d62bda37678f3e43ae55ce9f45cce14d71
Lílian Ferreira de Freitas <lily.mosquitoes@gmail.com>
2023-10-16 16:44:39 +0300

    feat: add commit hash to blame-line

--- nvim/lua/plugins/blame-line.lua
+++ nvim/lua/plugins/blame-line.lua
@@ -1,6 +1,9 @@
 return {
   'braxtons12/blame_line.nvim',
   config = function()
-    require('blame_line').setup()
+    require('blame_line').setup({
+      prefix = ' ',
+      template = "<commit-short> • <author> • <author-time> • <summary>",
+    })
   end
 }
